873d0b89c3b58e043e9453727e8c091edb8e2518
Jay Karippacheril Jacob <jkarippa@2-A-3.42heilbronn.de>
2025-10-19 20:30:02 +0200

    Rev.1.1 Revised test2.c to work such that there is no data race as was seen in the previous version. This was acheived using mutex to lock and unlock the data access based on the first come thread.

--- philo/test2.c
+++ philo/test2.c
@@ -6,7 +6,7 @@
 /*   By: jkarippa <jkarippa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/19 18:26:07 by jkarippa          #+#    #+#             */
-/*   Updated: 2025/10/19 20:00:30 by jkarippa         ###   ########.fr       */
+/*   Updated: 2025/10/19 20:27:32 by jkarippa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,30 +15,43 @@
 #include <stdlib.h>
 #define BIG 1000000000UL
 
+typedef struct s_utils
+{
+	__uint32_t		*a;
+	pthread_mutex_t	*a_lock;
+}					t_utils;
+
 void	*change_a(void *arg)
 {
-	__uint32_t	*a;
 	__uint32_t	i;
+	t_utils		*util;
 
-	a = (__uint32_t *)arg;
+	util = (t_utils *)arg;
 	i = 0;
+	pthread_mutex_lock(util->a_lock);
 	while (i < BIG)
 	{
-		(*a)++;
+		(*(util->a))++;
 		i++;
 	}
+	pthread_mutex_unlock(util->a_lock);
 	return (NULL);
 }
 
 int	main(void)
 {
-	pthread_t	thread_1;
-	pthread_t	thread_2;
-	__uint32_t	a;
+	pthread_t		thread_1;
+	pthread_t		thread_2;
+	__uint32_t		a;
+	t_utils			util;
+	pthread_mutex_t	a_lock;
 
 	a = 0;
-	pthread_create(&thread_1, NULL, change_a, &a);
-	pthread_create(&thread_2, NULL, change_a, &a);
+	util.a = &a;
+	pthread_mutex_init(&a_lock, NULL);
+	util.a_lock = &a_lock;
+	pthread_create(&thread_1, NULL, change_a, &util);
+	pthread_create(&thread_2, NULL, change_a, &util);
 	pthread_join(thread_1, NULL);
 	pthread_join(thread_2, NULL);
 	printf("a is: %u\n", a);
