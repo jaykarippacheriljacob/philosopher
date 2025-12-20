b80001ad8d3607428c4b0240dd385c2a05aedef2
Jay Karippacheril Jacob <jaykarippacheriljacob@gmail.com>
2025-12-20 18:13:00 +0100

    Rev.3.3 removed all the norm errors.

--- philo/utils_synchro_monitor.c
+++ philo/utils_synchro_monitor.c
@@ -6,41 +6,42 @@
 /*   By: jkarippa <jkarippa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/20 16:36:27 by jkarippa          #+#    #+#             */
-/*   Updated: 2025/12/20 17:53:59 by jkarippa         ###   ########.fr       */
+/*   Updated: 2025/12/20 18:12:19 by jkarippa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 
-void set_bool(t_mutex *mutex, bool *dest, bool value)
+void	set_bool(t_mutex *mutex, bool *dest, bool value)
 {
-    safe_mutex(&mutex->lock, LOCK);
-    *dest = value;
-    safe_mutex(&mutex->lock, UNLOCK);
+	safe_mutex(&mutex->lock, LOCK);
+	*dest = value;
+	safe_mutex(&mutex->lock, UNLOCK);
 }
 
-bool  get_bool(t_mutex *mutex, bool *src)
+bool	get_bool(t_mutex *mutex, bool *src)
 {
-    bool value;
+	bool	value;
 
-    safe_mutex(&mutex->lock, LOCK);
-    value = *src;
-    safe_mutex(&mutex->lock, UNLOCK);
-    return (value);
+	safe_mutex(&mutex->lock, LOCK);
+	value = *src;
+	safe_mutex(&mutex->lock, UNLOCK);
+	return (value);
 }
 
 /*
 ** SpinLock to synchornize all philosophers to start at the same time
 */
-while (true)
+void	wait_all_threads(t_table *table)
 {
-    pthread_mutex_lock(&table->table_mutex);
-    if (table->all_threads_ready)
-    {
-        pthread_mutex_unlock(&table->table_mutex);
-        break;
-    }
-    pthread_mutex_unlock(&table->table_mutex);
+	while (true)
+	{
+		pthread_mutex_lock(&table->table_mutex);
+		if (table->all_threads_ready)
+		{
+			pthread_mutex_unlock(&table->table_mutex);
+			break ;
+		}
+		pthread_mutex_unlock(&table->table_mutex);
+	}
 }
-
-
